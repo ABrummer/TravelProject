f62c8338815f67770ef3911300346978744f441f
AmandaBrummer <amanda@yonnot.com>
2022-06-01 15:05:34 -0700

    Initial Commit

---
+++ TravelProject/TravelProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelProjectApp.swift
+//  TravelProject
+//
+//  Created by Amanda Brummer on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
